6e06e04949b4ab1c7be190fe15b3378ab0c501d9
Matheus Fernandes <mcfernandes1110@gmail.com>
2019-12-07 10:59:55 -0300

    Day6 pt2

--- 2019/06.hs
+++ 2019/06.hs
@@ -17,9 +17,19 @@ module Day6 where
   distance _ "COM" = 1
   distance o a = 1 + (distance o $ o M.! a)
 
-  solve :: OrbitMap -> Int
-  solve os = sum $ distance os <$> os
+  part1 :: OrbitMap -> Int
+  part1 os = sum $ distance os <$> os
+
+  pathToCom :: OrbitMap -> String -> [String]
+  pathToCom om "COM" = ["COM"]
+  pathToCom om x = x:(pathToCom om $ om M.! x)
+
+  part2 :: OrbitMap -> Int
+  part2 os = ((length you) + (length santa) - 2 * (length diff)) - 2
+    where 
+      (you, santa) = (reverse $ pathToCom os "YOU", reverse $ pathToCom os "SAN")
+      diff = takeWhile (flip (elem) santa) you
 
   main = do
     text <- readFile "./Day6.txt"
-    print $ solve $ parse text
+    print $ part2 $ parse text
