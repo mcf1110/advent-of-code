8c226495e3c0715608d0df94f8b0b36732101977
Matheus Fernandes <mcfernandes1110@gmail.com>
2019-12-09 01:32:04 -0300

    Day 8 part 2

--- 2019/08.hs
+++ 2019/08.hs
@@ -11,7 +11,7 @@ module Day08 where
             size = w*h
     
     showL :: [Layer] -> String
-    showL l = unlines $ fmap unlines $ fmap (fmap show) l
+    showL l = unlines $ fmap unlines $ fmap (fmap (fmap toChar)) l
 
     countDigitsOnLayer :: Int -> Layer -> Int
     countDigitsOnLayer d = (sum . (fmap (length . (filter (==d)))))
@@ -19,9 +19,21 @@ module Day08 where
     layerOfFewestDigits :: Int -> [Layer] -> Int
     layerOfFewestDigits d ls = head $ elemIndices (minimum dCount) dCount
         where dCount = fmap (countDigitsOnLayer d) ls
+    
+    combinePixels :: Int -> Int -> Int
+    combinePixels 2 x = x
+    combinePixels x _ = x
+
+    combineLayers :: Layer -> Layer -> Layer
+    combineLayers = zipWith (zipWith combinePixels)
+
+    toChar :: Int -> Char
+    toChar 0 = ' '
+    toChar _ = '█'
 
     main = do
         str <- readFile "08.txt"
         let ls = (toLayers 25 6 str)
         let layer = ls !! (layerOfFewestDigits 0 ls)
         print $ (countDigitsOnLayer 1 layer) * (countDigitsOnLayer 2 layer)
+        putStr $ showL $ [foldl1 combineLayers ls]
