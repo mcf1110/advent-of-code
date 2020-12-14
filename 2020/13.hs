f14bb82d241f1a7e908a0376b39a0859e282f714
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-12-14 16:03:46 -0300

    new approach to day 13

--- 2020/13.hs
+++ 2020/13.hs
@@ -8,13 +8,18 @@ part1 target buses = busId * wait
         (busId, wait) = head $ sortBy (\(_, tw1) (_, tw2) -> compare tw1 tw2)  $ zip buses timeWaiting
         timeWaiting = zipWith (-) buses $ (target `mod`) <$> buses
 
-part2 buses = find isValid starts
-    where
-        offIds = tail $ map (fmap read) $ filter ((/= "x") . snd) $ zip [0..] buses :: [(Int, Int)]
-        base = read $ head buses
-        starts = map (*base) $ [0..]
-        isValid st = all (\(off, bid) -> (off + st) `mod` bid == 0) offIds
+-- Bruteforce approach
+-- part2 buses = find isValid starts
+--     where
+--         offIds = tail $ map (fmap read) $ filter ((/= "x") . snd) $ zip [0..] buses :: [(Int, Int)]
+--         base = read $ head buses
+--         starts = map (*base) $ [0..]
+--         isValid st = all (\(off, bid) -> (off + st) `mod` bid == 0) offIds
 
+part2 buses = fst $ foldl1 f offIds
+    where offIds = map (fmap read) $ filter ((/= "x") . snd) $ zip [0..] buses :: [(Int, Int)]
+          f (start, step) (off, bid) = (start', step*bid)
+              where start' = head $ filter (\x -> (x+off) `mod` bid == 0) $ [start + i*step | i<-[1..]]
 
 main = do
     inp <- lines <$> readFile "13.txt"
