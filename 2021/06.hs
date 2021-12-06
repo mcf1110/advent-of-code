4c0a64ca569e3936b424e43efb3c1c9255bf2090
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-12-06 10:50:05 -0300

    06-2021

---
+++ 2021/06.hs
@@ -0,0 +1,30 @@
+module Day06 where
+
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Control.Monad (forM_)
+
+p :: [Int]
+p = [3,4,3,1,2]
+
+main :: IO ()
+main = do
+  input <- readFile "06.txt"
+  let vec = toVector $ (read $ "["<>input<>"]" :: [Int])
+  print $ sum $ (iterate step vec) !! 80
+  print $ sum $ (iterate step vec) !! 256
+
+toVector :: [Int] -> V.Vector Int
+toVector xs = go (V.replicate 9 0) xs
+    where 
+        go v [] = v
+        go v (x:xs) = go (V.modify (\vec -> MV.modify vec succ x) v) xs
+         
+
+step :: V.Vector Int -> V.Vector Int
+step v = V.modify action v
+    where 
+        parents = V.head v
+        action vec = do
+            forM_ [0..7] (\i -> MV.swap vec i (i+1)) -- bubbles up 0-th element to the end
+            MV.modify vec (+parents) 6
